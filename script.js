b8c2b8aee1ebc6b507bf4796550af94e669f32ae
Ilya <skolto@ya.ru>
2019-04-21 19:29:04 +0300

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var string = "Привет!";
+console.log(4/0);
+
+var obj = {};
+
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMariied: false
+};
+
+console.log(persone["name"]);
